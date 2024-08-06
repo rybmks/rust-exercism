4934e36b3a2b2b9c775e7da552e821ba3cb3722d
Maksym <159819659+rybmks@users.noreply.github.com>
2024-08-06 21:19:39 +0300

    Update gigasecond.rs

--- src/gigasecond.rs
+++ src/gigasecond.rs
@@ -1,7 +1,7 @@
 use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
-const DURATION: time::Duration = Duration::new(1_000_000_000, 0);
+const GIGASECOND: time::Duration = Duration::new(1_000_000_000, 0);
 pub fn after(start: DateTime) -> DateTime {
-    start + DURATION
+    start + GIGASECOND
 }
